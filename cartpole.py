a5d9746e96c74c836e499777b3c76cc7f2b2fcf3
DJ <dwiebold7@gmail.com>
2018-02-27 12:14:38 -0700

    adding cartpole

---
+++ cartpole.py
@@ -0,0 +1,51 @@
+import gym
+import numpy
+import BuildNeuralNet
+env = gym.make('CartPole-v1')
+# env.reset()
+# global fitness
+# fitness = 0
+# for x in range(1000):
+#     env.render()
+#     action = x % 2
+#     print(env.action_space)
+#     observation, reward, done, info = env.step(action)
+#     fitness += reward
+#     print("Obv:", observation)
+#     print("Reward:", fitness)
+#     if done:
+#         break
+
+
+def get_xy():
+    env.reset()
+    for x in range(1000):
+        #env.render()
+        #action = x % 2
+        #print(env.action_space)
+        action = x % 2
+        observation, reward, done, info = env.step(action)
+        #fitness += reward
+        #print("Obv:", observation)
+        #print("Reward:", fitness)
+        x = str(env.action_space)
+        x = (x[9]) # action space returns 'Discrete(#)' and we just want the #
+        return len(observation), int(x)
+
+def get_fitness(NN):
+    observation = env.reset()
+    print((observation))
+    reward = 0
+    for x in range(1000):
+        observation = observation.tolist()
+        observation.append(1)
+        print(observation)
+        NN.predict(observation)
+        if NN.output[0] == 1:
+            action = 0
+        else:
+            action = 1
+        observation, reward, done, info = env.step(action)
+        reward += reward
+        if done:
+            return reward
