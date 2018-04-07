2ef98fdb027756b91c8b716f79399256d1fd893b
DJ <dwiebold7@gmail.com>
2018-04-06 19:19:59 -0600

    refactor

--- Population.py
+++ Population.py
@@ -161,8 +161,8 @@ def get_delta(genome, genome2):
     else:
         N = 1
 
-    whatever = [abs(g1con.weight - g2con.weight) for g1con in genome.connections for g2con in genome2.connections if g1con.innovation == g2con.innovation]
-    W = sum(whatever)/len(whatever)
+    list_weight_diffs = [abs(g1con.weight - g2con.weight) for g1con in genome.connections for g2con in genome2.connections if g1con.innovation == g2con.innovation]
+    W = sum(list_weight_diffs)/len(list_weight_diffs)
     num_similarities = sum([1 for x in genome.connections for y in genome2.connections if x.innovation == y.innovation])
     E = len(genome.connections) + len(genome2.connections) - 2 * num_similarities
 
